9d88bc89bf1aa155d3213d63466973eb69a806da
Joshua Gomez <jngomez@gwu.edu>
2013-05-14 17:03:02 -0400

    Significant changes to the argument parsing of clint. It now supports nested parsers so we can write out commands like
    clint add bag
    
    or
    
    clint edit item --title 'Django Shoots First'
    
    The nested parsers allow each type of item to have its own argument list.
    
    Also changed some of the inventory.py models to better support option lists.

--- clint.py
+++ clint.py
@@ -3,6 +3,7 @@
 import argparse
 
 from inventory import Machine, Collection, Project, Item, Bag, _delete
+from inventory import Inventory404
 import settings
 
 
@@ -26,95 +27,234 @@ move <id>
 validate <id>
 '''
 
-def show(model, id):
+
+def show(args):
     try:
-        obj = globals()[model.capitalize()](id)
+        obj = globals()[args.model.capitalize()](args.id)
         print obj.to_string()
     except Inventory404, e:
-        print 'No record found for %s %s' % (model, id)
+        print 'No record found for %s %s' % (args.model, args.id)
     except Exception, e:
         print 'Error fetching data!\n', e
 
 
-def add(model):
+def add(args):
     try:
-        obj = build_new_obj(model)
-        print '%s Created!\n' % model.capitalize()
+        obj = globals()[args.model.capitalize()]()
+        vals = [a for a in obj.readwrite() if getattr(args, a, None) is not None]
+        for attr in vals:
+            setattr(obj, attr, getattr(args, attr))
+        # if no optional args passed, get metadata from user
+        if vals == []:
+            user_build_new_obj(obj, args.model)
+        obj.save()
+        print '%s Created!\n' % args.model.capitalize()
         print obj.to_string()
     except Exception, e:
         print 'Error creating record!\n', e
         raise
 
 
-def edit(model, id, **kwargs):
+def edit(args):
     try:
-        obj = globals()[model.capitalize()](id)
-        edit_obj(obj)
-        print '\n%s Edited!\n' % model.capitalize()
+        obj = globals()[args.model.capitalize()](args.id)
+        print obj.to_string()
+        edits = [a for a in obj.readwrite() if getattr(args, a, None) is not None]
+        for attr in edits:
+            setattr(obj, attr, getattr(args, attr))
+        # if no optional args passed, get metadata from user
+        if edits == []:
+            user_edit_obj(obj)
+        obj.save()
+        print '\n%s Edited!\n' % args.model.capitalize()
         print obj.to_string()
     except Inventory404, e:
-        print 'No record found for %s %s' % (model, id)
+        print 'No record found for %s %s' % (args.model, args.id)
     except Exception, e:
         print 'Error editing record!\n', e
+        raise
 
 
-def delete(model, id):
-    response = _delete(model, id)
+def delete(args):
+    response = _delete(args.model, args.id)
     if response.status_code == 204:
-        print 'Successful deletion of %s %s' % (model, id)
+        print 'Successful deletion of %s %s' % (args.model, args.id)
     else:
-        print 'Error deleting %s %s' % (model, id)
+        print 'Error deleting %s %s' % (args.model, args.id)
 
 
-def build_new_obj(model):
-    obj = globals()[model.capitalize()]()
+def user_build_new_obj(obj, model):
     print 'Enter field values for new %s' % model
     for attr, opts in obj.writeopts():
         if opts:
-            optlist = ', '.join(['%s=%s' % (k, v) for k, v in opts])
+            optlist = ', '.join(['%s=%s' % (k, v) for k, v in opts.items()])
             prompt = '%s [Options: %s]: ' % (attr, optlist)
         else:
             prompt = '%s: ' % attr
         value = raw_input(prompt)
         setattr(obj, attr, value)
-    return obj.save()
 
-def edit_obj(obj):
-    print obj.to_string()
+
+def user_edit_obj(obj):
     print '\nEditable Fields'
     for attr, opts in obj.writeopts():
         if opts:
-            optlist = ', '.join(['%s=%s' % (k, v) for k, v in opts])
+            optlist = ', '.join(['%s=%s' % (k, v) for k, v in opts.items()])
             prompt = '%s [Options: %s]: ' % (attr, optlist)
         else:
             prompt = '%s: ' % attr
         value = raw_input(prompt)
         setattr(obj, attr, value)
-    return obj.save()
+
 
 def main():
 
-    actions = ['show', 'add', 'edit', 'delete', 'list']
+    # reference variables
     models = ['machine', 'collection', 'project', 'item', 'bag']
+    orig_item_types = ['book', 'microfilm', 'audio', 'video', 'mixed',
+        'other']
+    bag_types = ['access', 'preservation', 'export']
 
+    # create clint level parser
     parser = argparse.ArgumentParser(
         description='A command line tool for Inventory operations')
-    parser.add_argument('action', choices=actions,
-        help='action or subcommand to perform')
-    parser.add_argument('model', choices=models,
-        help='type of object to be acted on')
-    parser.add_argument('-i', '--id', default=None,
-        help='identifier of the object [optional for the "add" command]')
+    subparsers = parser.add_subparsers()
+    # add subparsers for each command
 
-    try:
-        args = parser.parse_args()
-        kwargs = {}
-        keys = [k for k in vars(args).keys() if getattr(args, k) and k != 'action']
-        for key in keys:
-                kwargs[key] = getattr(args, key)
-        globals()[args.action](**kwargs)
-    except:
-        raise
+    # parser for the "show" command
+    show_parser = subparsers.add_parser('show',
+        help='Display metadata for an Inventory object')
+    show_parser.add_argument('model', choices=models,
+        help='type of object to be acted on')
+    show_parser.add_argument('id', help='identifier of the object')
+    show_parser.set_defaults(func=show)
+
+    # parser for the "add" command
+    add_parser = subparsers.add_parser('add',
+        help='Add a new object to the Inventory')
+    add_parser.set_defaults(func=add)
+    # add subsubparser for various object types in "add" command
+    addsubpar = add_parser.add_subparsers()
+    # add collection
+    addc = addsubpar.add_parser('collection',
+        help='Add a collection to Inventory')
+    addc.add_argument('-n', '--name', help='Name of the Collection')
+    addc.add_argument('-d', '--description',
+        help='Description of the Collection')
+    addc.add_argument('-m', '--manager', help='Manager of the Collection')
+    addc.add_argument('--model', default='collection')
+    # add project
+    addp = addsubpar.add_parser('project', help='Add a project to Inventory')
+    addp.add_argument('-n', '--name', help='Name of the project')
+    addp.add_argument('-m', '--manager', help='Manager of the project')
+    addp.add_argument('-c', '--collection',
+        help='ID of the collection this project feeds')
+    addp.add_argument('-s', '--start-date', help='Start date of the project')
+    addp.add_argument('-e', '--end-date', help='End date of the project')
+    addp.add_argument('--model', default='project')
+    # add item
+    addi = addsubpar.add_parser('item', help='Add an item to Inventory')
+    addi.add_argument('-t', '--title', help='Title of the item')
+    addi.add_argument('-l', '--localid', help='Alt/local ID of the item')
+    addi.add_argument('-p', '--project',
+        help='Project this item is associated with')
+    addi.add_argument('-c', '--collection',
+        help='Collection this item is associated with')
+    addi.add_argument('-o', '--original-item-type', choices=orig_item_types,
+        help='The type of object this digital item came from')
+    addi.add_argument('-n', '--notes', help='Notes about the item')
+    addi.add_argument('--model', default='item')
+    # add bag
+    addb = addsubpar.add_parser('bag', help='Add a bag to the Inventory')
+    addb.add_argument('-n', '--bagname', help='Identifier/name of the bag')
+    addb.add_argument('-t', '--bagtype', choices=bag_types,
+        help='Type of bag')
+    addb.add_argument('-p', '--path', help='Path to bag from server root')
+    addb.add_argument('-y', '--payload', help='Payload of the bag')
+    addb.add_argument('-m', '--machine', help='Machine this bag is stored on')
+    addb.add_argument('-i', '--item', help='Item this bag is associated with')
+    addb.add_argument('-c', '--created',
+        help='Timestamp when this bag was created')
+    addb.add_argument('--model', default='bag')
+    # add machine
+    addm = addsubpar.add_parser('machine',
+        help='Add a machine to the Inventory')
+    addm.add_argument('-n', '--name', help='Name of the machine')
+    addm.add_argument('-u', '--url', help='URL of the machine')
+    addm.add_argument('--model', default='machine')
+
+    # parser for the "edit" command
+    edit_parser = subparsers.add_parser('edit',
+        help='Edit the metadata of an Inventory object')
+    edit_parser.set_defaults(func=edit)
+    # add subsubparser for various object types in "edit" command
+    editsubpar = edit_parser.add_subparsers()
+    # edit collection
+    editc = editsubpar.add_parser('collection',
+        help='Edit a collection in the Inventory')
+    editc.add_argument('id', help='identifier of the collection')
+    editc.add_argument('-n', '--name', help='Name of the Collection')
+    editc.add_argument('-d', '--description',
+        help='Description of the Collection')
+    editc.add_argument('-m', '--manager', help='Manager of the Collection')
+    editc.add_argument('--model', default='collection')
+    # edit project
+    editp = editsubpar.add_parser('project',
+        help='Edit a project in the Inventory')
+    editp.add_argument('id', help='identifier of the project')
+    editp.add_argument('-n', '--name', help='Name of the project')
+    editp.add_argument('-m', '--manager', help='Manager of the project')
+    editp.add_argument('-c', '--collection',
+        help='ID of the collection this project feeds')
+    editp.add_argument('-s', '--start-date', help='Start date of the project')
+    editp.add_argument('-e', '--end-date', help='End date of the project')
+    editp.add_argument('--model', default='project')
+    # edit item
+    editi = editsubpar.add_parser('item',
+        help='Edit an item in the Inventory')
+    editi.add_argument('id', help='identifier of the item')
+    editi.add_argument('-t', '--title', help='Title of the item')
+    editi.add_argument('-l', '--localid', help='Alt/local ID of the item')
+    editi.add_argument('-p', '--project',
+        help='Project this item is associated with')
+    editi.add_argument('-c', '--collection',
+        help='Collection this item is associated with')
+    editi.add_argument('-o', '--original-item-type', choices=orig_item_types,
+        help='The type of object this digital item came from')
+    editi.add_argument('-n', '--notes', help='Notes about the item')
+    editi.add_argument('--model', default='item')
+    # edit bag
+    editb = editsubpar.add_parser('bag', help='Edit a bag in the Inventory')
+    editb.add_argument('-n', '--bagname', help='Identifier/name of the bag')
+    editb.add_argument('-t', '--bagtype', choices=bag_types,
+        help='Type of bag')
+    editb.add_argument('-p', '--path', help='Path to bag from server root')
+    editb.add_argument('-y', '--payload', help='Payload of the bag')
+    editb.add_argument('-m', '--machine', help='Machine this bag is stored on')
+    editb.add_argument('-i', '--item', help='Item this bag is associated with')
+    editb.add_argument('-c', '--created',
+        help='Timestamp when this bag was created')
+    editb.add_argument('--model', default='bag')
+    # edit machine
+    editm = editsubpar.add_parser('machine',
+        help='Edit a machine in the Inventory')
+    editm.add_argument('id', help='identifier of the machine')
+    editm.add_argument('-n', '--name', help='Name of the machine')
+    editm.add_argument('-u', '--url', help='URL of the machine')
+    editm.add_argument('--model', default='machine')
+
+    # parser for the "delete" command
+    del_parser = subparsers.add_parser('delete',
+        help='Delete an object from the Inventory')
+    del_parser.add_argument('model', choices=models,
+        help='type of object to be acted on')
+    del_parser.add_argument('id', default=None,
+        help='identifier of the object')
+    del_parser.set_defaults(func=delete)    
+
+    args = parser.parse_args()
+    args.func(args)
+
 
 if __name__ == '__main__':
     main()
