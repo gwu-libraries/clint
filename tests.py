0ce4ba26ec2874051c7a8d969fa49cb1b7e63dfa
Joshua Gomez <jngomez@gwu.edu>
2013-06-03 16:56:39 -0400

    Adjusted existing tests to new inventory base http methods

--- tests.py
+++ tests.py
@@ -1,14 +1,17 @@
 import unittest
+from unittest import skipIf
 
-from inventory import Inventory
+import inventory
+import settings
 from settings import inventory_sandbox as sandbox
 
 
-class TestInventoryClass(unittest.TestCase):
+settings.INVENTORY_CREDENTIALS = sandbox
 
-    def setUp(self):
-        self.inv = Inventory(sandbox['user'], sandbox['apikey'],
-            sandbox['apiversion'], sandbox['url'], sandbox['port'])
+
+@skipIf(not all([sandbox.get(k) for k in sandbox.keys()]),
+    'sandbox inventory not set')
+class TestInventoryHTTPMethods(unittest.TestCase):
 
     def testget(self):
         '''
@@ -17,22 +20,22 @@ class TestInventoryClass(unittest.TestCase):
         $: python manage loaddata fake_stuff.json
         '''
         # get a collection
-        res1 = self.inv.get('collection', '12345/c00000000001')
+        res1 = inventory._get('collection', '12345/c00000000001')
         self.assertEqual(res1.status_code, 200)
         j1 = res1.json()
         self.assertEqual(j1['name'], 'Fake Collection 1')
         # get a project
-        res2 = self.inv.get('project', '12345/p000000001')
+        res2 = inventory._get('project', '12345/p000000001')
         self.assertEqual(res2.status_code, 200)
         j2 = res2.json()
         self.assertEqual(j2['name'], 'Fake Project 1')
         # get an item
-        res3 = self.inv.get('item', '12345/i000000031')
-        self.assertEqual(res1.status_code, 200)
+        res3 = inventory._get('item', '12345/i000000031')
+        self.assertEqual(res3.status_code, 200)
         j3 = res3.json()
         self.assertEqual(j3['title'], 'Fake Audio 1')
         # get a bag
-        res4 = self.inv.get('bag', '12345/i0000000031_ACCESS_BAG')
+        res4 = inventory._get('bag', '12345/i0000000031_ACCESS_BAG')
         self.assertEqual(res4.status_code, 200)
         j4 = res4.json()
         self.assertEqual(j4['item'], '/api/v1/item/12345/i000000031/')
@@ -42,25 +45,24 @@ class TestInventoryClass(unittest.TestCase):
         cdata = {"id": "12345/100000000001", "name": "Test Collection 1",
             "description": "A test collection", "manager": "Joshua Gomez",
             "created": "2013-04-25 14:19:01.351058"}
-        response1 = self.inv.post('collection', **cdata)
+        response1 = inventory._post('collection', **cdata)
         self.assertEqual(response1.status_code, 201)
         # now delete it
-        response2 = self.inv.delete('collection', '12345/100000000001')
+        response2 = inventory._delete('collection', '12345/100000000001')
         self.assertEqual(response2.status_code, 204)
 
     def testbadpost(self):
-        # create a collection with no name or create date
-        cdata = {"id": "12345/c10000000001", "manager": "Joshua Gomez",
+        # create a collection with a name that is too long
+        name = 'name' * 65
+        cdata = {"id": "", "manager": "Joshua Gomez", "name": name,
             "description": "A test collection"}
-        response1 = self.inv.post('collection', **cdata)
-        self.assertEqual(response1.status_code, 400)
-        # now delete it
-        #response2 = self.inv.delete('collection', '12345/100000000001')
-        #self.assertEqual(response2.status_code, 204)
+        response1 = inventory._post('collection', **cdata)
+        self.assertEqual(response1.status_code, 500)
+        # That should really be a 400, but tatypie returns a 500, patch pending
 
     def testpatch(self):
         cdata = {'description': 'Patch Worked!'}
-        response = self.inv.patch('collection', '12345/c00000000001', **cdata)
+        response = inventory._patch('collection', '12345/c00000000001', **cdata)
         self.assertEqual(response.status_code, 202)
 
 if __name__ == '__main__':
